8bdf70fb49c529dbd830612f5d0e9f193504e39d
mitchabaza <mitch.abaza@gmail.com>
2015-02-14 17:05:52 -0600

    commit

--- WebService/Global.asax.cs
+++ WebService/Global.asax.cs
@@ -19,7 +19,6 @@ namespace WebService
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-            WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
           
